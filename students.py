d0a9924ec66d165b1964f2982bcf33c34ba3ec7d
Nishant <nishant15066@iiitd.ac.in>
2018-01-29 16:29:53 +0530

    Work done

--- students.py
+++ students.py
@@ -1,20 +1,21 @@
 import csv
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 # Read CSV file
-with open('s.csv', 'r') as fp:
+with open('students.csv', 'r') as fp:
     reader = csv.reader(fp, delimiter=',', quotechar='"')
     # next(reader, None)  # skip the headers
     data_read = [row for row in reader]
 x = 0
-dic = {}
 for row in data_read:
     row.pop(0)
-data_read.pop(2)
 
 header = data_read[0]
-
 data = data_read
 data.pop(0)
 
+dic = {}
 for i in header:
     dic[i] = {}
     dic[i][''] = 0
@@ -24,9 +25,6 @@ for i in header:
     dic[i]['3'] = 0
     dic[i]['x'] = 0
 
-# for i in dic.keys():
-#     print i, dic[i]
-
 for i in range(len(header)):
     for row in data:
         if row[i] in dic[header[i]]:
@@ -34,26 +32,30 @@ for i in range(len(header)):
         else:
             dic[header[i]]['x'] += 1
 
-# for i in dic.keys():
-#     print i, dic[i]
-
-x = dic.keys()
+x = list(dic.keys())
 x.sort()
 
 ans = []
-for i in x:
+labels = ['Yes(1)', 'No(2)', 'Other']
+
+# Sent files till 12
+for j in range(12):
+    i = x[j]
     temp = []
-    temp.append(i)
+    # temp.append(i)
     # temp.append(dic[i][''])
     # temp.append(dic[i]['0'])
     temp.append(dic[i]['1'])
     temp.append(dic[i]['2'])
-    # temp.append(dic[i]['3'])
+    temp.append(dic[i]['3'] + dic[i]['x'] + dic[i][''] )
+    trace = go.Pie( labels = labels, values = temp )
+    py.plot([trace], filename = str("Students_" + i) )
     # temp.append(dic[i]['x'])
-    ans.append(temp)
-    print temp
-
-with open('test.csv', 'w') as fp:
-    writer = csv.writer(fp, delimiter=',')
-    writer.writerow(["Question","1", "2"])  # write header
-    writer.writerows(ans)
+    # ans.append(temp)
+    print(temp)
+#
+# with open('output.csv', 'w') as fp:
+#     writer = csv.writer(fp, delimiter=',')
+#     writer.writerow(["Question","1", "2", "Others"])  # write header
+#     writer.writerows(ans)
+#
